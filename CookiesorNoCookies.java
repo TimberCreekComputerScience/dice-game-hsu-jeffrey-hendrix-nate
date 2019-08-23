c1d699b10a4cf7c15a933e75942c9ba9e2ac9aab
natehendrix <54180104+natehendrix@users.noreply.github.com>
2019-08-23 07:15:03 -0400

    Add files via upload

---
+++ CookiesorNoCookies.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CookiesorNoCookies {
+	
+	public static void main(String[] args) {
+		Scanner Dealer = new Scanner(System.in);
+		System.out.print("Press Enter to Start");
+		String Enter = Dealer.nextLine();
+		int Dice43 = (int)(Math.random()*6)+1;
+		int Dice27261 = (int)(Math.random()*6)+1;
+		if (Dice43 == Dice27261) 
+			System.out.println("Congrats! You did it! you got "+Dice43+" and "+Dice27261+"!"+"\n However, you do not get any cookies :(");
+		else {
+			System.out.println("Congrats! Press enter to continue.");
+		String YouSuck = Dealer.nextLine();
+		System.out.println("You actually lost. Too bad, no cookies for you! \n Your results were "+Dice43+" and "+Dice27261+" );");}
+
+	}
+
+}
