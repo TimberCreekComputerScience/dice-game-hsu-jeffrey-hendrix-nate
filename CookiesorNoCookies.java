a4e1e0ef6a51c13b29039d58a77d53a2b3c9d34b
Jeffrey Hsu <54180253+WolfO9ofWPA@users.noreply.github.com>
2019-08-25 16:03:17 -0400

    Update CookiesorNoCookies.java

--- CookiesorNoCookies.java
+++ CookiesorNoCookies.java
@@ -7,8 +7,10 @@ public class CookiesorNoCookies {
 		String Enter = Dealer.nextLine();
 		int Dice43 = (int)(Math.random()*6)+1;
 		int Dice27261 = (int)(Math.random()*6)+1;
-		if (Dice43 == Dice27261) 
-			System.out.println("Congrats! You did it! you got "+Dice43+" and "+Dice27261+"!"+"\n However, you do not get any cookies :(");
+		if (Dice43 == Dice27261) {
+			System.out.println("Congrats! Press enter to continue.");
+			String YouSuck = Dealer.nextLine();
+			System.out.println("Congrats! You did it! you got "+Dice43+" and "+Dice27261+"!"+"\n However, you do not get any cookies :(");}
 		else {
 			System.out.println("Congrats! Press enter to continue.");
 		String YouSuck = Dealer.nextLine();
